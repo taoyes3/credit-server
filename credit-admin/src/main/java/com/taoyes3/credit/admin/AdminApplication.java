f8164be7b233e95575b538c6247e596faceed1af
taoyes003 <taoyes003@gmail.com>
2022-09-14 17:28:51 +0800

    project init

---
+++ credit-admin/src/main/java/com/taoyes3/credit/admin/AdminApplication.java
@@ -0,0 +1,11 @@
+package com.taoyes3.credit.admin;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author taoyes3
+ * @date 2022/9/14 17:25
+ */
+@SpringBootApplication
+public class AdminApplication {
+}
