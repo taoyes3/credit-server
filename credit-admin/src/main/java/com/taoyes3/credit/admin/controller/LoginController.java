c5828ad1efd9a642cc1332a0a4cfa6edc64354b9
taoyes003 <taoyes003@gmail.com>
2022-09-22 21:40:18 +0800

    login module

--- credit-admin/src/main/java/com/taoyes3/credit/admin/controller/LoginController.java
+++ credit-admin/src/main/java/com/taoyes3/credit/admin/controller/LoginController.java
@@ -1,12 +1,18 @@
 package com.taoyes3.credit.admin.controller;
 
+import com.anji.captcha.model.common.ResponseModel;
+import com.anji.captcha.model.vo.CaptchaVO;
+import com.anji.captcha.service.CaptchaService;
+import com.taoyes3.credit.common.exception.CreditBindException;
 import com.taoyes3.credit.security.admin.dto.CaptchaAuthenticationDTO;
+import com.taoyes3.credit.sys.service.SysUserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
 import javax.validation.Valid;
 
 /**
@@ -17,8 +23,19 @@ import javax.validation.Valid;
 @RequestMapping("/login")
 @Slf4j
 public class LoginController {
+    @Resource
+    private CaptchaService captchaService;
+    @Resource
+    private SysUserService sysUserService;
+    
     @PostMapping
     public void store(@Valid @RequestBody CaptchaAuthenticationDTO captchaAuthenticationDTO) {
-        
+        // 登陆后台登录需要再校验一遍验证码
+        CaptchaVO captchaVO = new CaptchaVO();
+        captchaVO.setCaptchaVerification(captchaAuthenticationDTO.getCaptchaVerification());
+        ResponseModel responseModel = captchaService.verification(captchaVO);
+        if (!responseModel.isSuccess()) {
+            throw new CreditBindException("验证码有误或已过期");
+        }
     }
 }
--- credit-security/credit-security-admin/src/main/java/com/taoyes3/credit/security/admin/dto/CaptchaAuthenticationDTO.java
+++ credit-security/credit-security-admin/src/main/java/com/taoyes3/credit/security/admin/dto/CaptchaAuthenticationDTO.java
@@ -2,11 +2,13 @@ package com.taoyes3.credit.security.admin.dto;
 
 import com.taoyes3.credit.security.common.dto.AuthenticationDTO;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 /**
  * @author taoyes3
  * @date 2022/9/22 15:19
  */
+@EqualsAndHashCode(callSuper = true)
 @Data
 public class CaptchaAuthenticationDTO extends AuthenticationDTO {
     private String captchaVerification;
